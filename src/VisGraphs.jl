048b7e6d7fc6e463100a5ba6bd58353769f8c881
Mohamed Aziz Ben Aissa <42911664+tpmdjd123456@users.noreply.github.com>
2026-06-25 15:35:11 +0200

    Add exports for whvg and wnvg functions

--- src/VisGraphs.jl
+++ src/VisGraphs.jl
@@ -1,6 +1,7 @@
 module VisGraphs
 
 using Plots
+using Plots.Colors: RGB
 
 include("core/hvg.jl")
 include("core/nvg.jl")
@@ -8,5 +9,6 @@ include("datasets/signals.jl")
 
 export generate_sine, generate_random, generate_noisy_sine
 export hvg, plot_hvg, nvg, plot_nvg
+export whvg, plot_whvg, wnvg, plot_wnvg
 
 end
